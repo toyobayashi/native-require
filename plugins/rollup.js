b21820ebbe26512c29ad1a1f493562ef632085f8
toyobayashi <lifenglin314@outlook.com>
2021-05-08 14:33:48 +0800

    3.0.2

--- plugins/rollup.js
+++ plugins/rollup.js
@@ -2,7 +2,8 @@ exports.nativeRequireRollupPlugin = function () {
   return {
     name: 'native-require',
     renderChunk (code /*, chunk, options */) {
-      return code.replace(/function\s+commonjsRequire\s+\(\)\s+\{\s*(\r?\n)*throw new Error\(['"]Dynamic requires are not currently supported by @rollup\/plugin-commonjs['"]\);\s*(\r?\n)*\s*}/g, '')
+      return code.replace(/function\s+commonjsRequire\s+\(\)\s+\{\s*(\r?\n)*\s*throw new Error\(['"]Dynamic requires are not currently supported by @rollup\/plugin-commonjs['"]\);\s*(\r?\n)*\s*\}/g, '')
+        .replace(/function\s+commonjsRequire\s+\((.+?)\)\s+\{\s*(\r?\n)*\s*throw new Error\(['"]Could not dynamically require ['"](.+?)\);\s*(\r?\n)*\s*\}/g, '')
         .replace(/commonjsRequire/g, 'require')
     }
   }
